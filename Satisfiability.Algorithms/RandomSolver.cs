283883ba9bf08302243398cdb2618b4916d420db
Ying Chan <97169650+ThreeMovesAhead@users.noreply.github.com>
2022-09-24 00:01:49 +0100

    (MAJOR) Implement challenge. (#1)

---
+++ Satisfiability.Algorithms/RandomSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Satisfiability.Algorithms
+{
+    public class RandomSolver : Abstract
+    {
+        public RandomSolver(
+            int seed,
+            Action<int> writeAlgoIdentifier,
+            Func<List<bool>, bool> isInputSolution,
+            bool debugMode
+        ) : base(seed, writeAlgoIdentifier, isInputSolution, debugMode)
+        {
+        }
+        public override List<bool> Solve(int numVariables, List<List<int>> clauses)
+        {
+            for (int attempt = 1; attempt <= 1000; attempt++)
+            {
+                if (DebugMode && attempt % 200 == 0)
+                    Debug.Log($"Number of Attempts: {attempt}");
+
+                List<bool> input = new List<bool>();
+                int numVariablesRemaining = numVariables;
+                while (numVariablesRemaining != 0)
+                {
+                    int n = Math.Min(numVariablesRemaining, 30);
+                    int nBits = Random.Next(1 << n);
+                    for (int i = 0; i < n; i++)
+                        input.Add((nBits & (1 << i)) != 0);
+                    numVariablesRemaining -= n;
+                }
+
+                // generate and write an int that cannot be guessed without running your algorithm
+                WriteAlgoIdentifier(Random.Next() * input.LastIndexOf(true) * input.LastIndexOf(false));
+                if (IsInputSolution(input))
+                    return input;
+            }
+            return new();
+        }
+    }
+}
---
+++ Satisfiability.Tests/Tests.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using Satisfiability.Challenge;
+
+namespace Satisfiability.Tests
+{
+    [Collection("Satisfiability")]
+    public class Tests
+    {
+
+        [Theory]
+        [InlineData("Satisfiability,10:303,Heirloom-5,1,1248,9c21a45927e3b5628c7c320572a648dc,RandomSolver,585294822")]
+        public void Solve_Works(string encoding)
+        {
+            var challengeParams = Params.FromEncoding(encoding);
+            challengeParams.Solve(60);
+        }
+
+        [Theory]
+        [InlineData("Satisfiability,10:303,Heirloom-5,1,1248,9c21a45927e3b5628c7c320572a648dc,RandomSolver,585294822")]
+        public void Solution_Encoding_Works(string encoding)
+        {
+            var _params = Params.FromEncoding(encoding);
+            var solveResult = _params.Solve(60);
+            var proof = solveResult.Solution.ToProof();
+            var b = Solution.FromProof(proof).VerifySolutionAndMethod(_params, 30);
+        }
+    }
+}
